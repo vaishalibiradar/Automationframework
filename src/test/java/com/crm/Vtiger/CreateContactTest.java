376dc18d0c89aeb5d046f0f4c51e557a7b790d86
Lenovo <Lenovo@Lenovo-PC>
2022-03-24 15:05:15 +0530

    user :rameshwar sahare has made changes successfully

--- src/test/java/com/crm/Vtiger/CreateContactTest.java
+++ src/test/java/com/crm/Vtiger/CreateContactTest.java
@@ -7,6 +7,8 @@ public class CreateContactTest {
 public void createContact() {
 	System.out.println("user 1 created contact");
 
+	System.out.println("Rameshwar sahare has done changes sucessfully");
+	
 	
 }
 }
