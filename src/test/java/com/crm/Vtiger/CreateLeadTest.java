4986b3c7d34de06fc4955c3f49d20b86ed2e6e51
radha <radha.sg2014@gmail.com>
2022-03-24 15:49:10 +0530

    modification has done

--- src/test/java/com/crm/Vtiger/CreateLeadTest.java
+++ src/test/java/com/crm/Vtiger/CreateLeadTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class CreateLeadTest {
 	@Test
-	public void createLed() {
+	public void createLead() {
 		System.out.println("user 1 created lead");
 		
 		System.out.println("user 2 created");
