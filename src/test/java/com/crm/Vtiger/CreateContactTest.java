943d9e5dc83c2a34009e972d30d6707125ab08ff
Vaishali <Vaishali@DESKTOP-2F977B9>
2022-03-28 16:51:24 +0530

    commit

--- src/test/java/com/crm/Vtiger/CreateContactTest.java
+++ src/test/java/com/crm/Vtiger/CreateContactTest.java
@@ -8,8 +8,8 @@ public void createContact() {
 	System.out.println("user 1 created contact");
 
     System.out.println("user 2 created contact");
-
-
+    
+    System.out.println("modified contact");
 
 	System.out.println("Rameshwar sahare has done changes sucessfully");
 	
